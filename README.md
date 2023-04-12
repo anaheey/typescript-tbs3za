43f52b4fad652d86e2db483d81d5617506917bba
anaheey <130571829+anaheey@users.noreply.github.com>
2023-04-12 15:56:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-tbs3za
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-tbs3za)
---
+++ index.ts
@@ -0,0 +1,8 @@
+let num: number[];
+let i,j,s:number;
+num=[];
+s=0;
+for(i=0;i<5;i++){
+  num[i]=parseInt(prompt("nu"));45
+}
+
